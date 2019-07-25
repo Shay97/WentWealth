79ef0f985be8fd8e2b2be66ee31b1caa4f1201aa
Charlie Pluk <charliepluk@users.noreply.github.com>
2019-07-25 19:14:34 -0400

    added files to project
    working on layout

---
+++ Development/app/src/main/java/edu/wit/mobileapp/wentwealth/Balance.java
@@ -0,0 +1,23 @@
+package edu.wit.mobileapp.wentwealth;
+
+public class Balance {
+
+    private double bal;
+
+    // Blank Constructor
+    private Balance()
+    {
+
+    }
+
+    // Getter and Setters
+    public void setBal(double bal)
+    {
+        this.bal = bal;
+    }
+
+    public double getBal()
+    {
+        return bal;
+    }
+}
